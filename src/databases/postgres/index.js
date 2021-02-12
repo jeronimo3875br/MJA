0602b39fec3666f788be9a7fa7d360a99821ecc3
= <jeroninhoxtreme@gmail.com>
2021-02-12 16:42:18 -0300

    first commit

--- src/databases/postgres/index.js
+++ src/databases/postgres/index.js
@@ -2,35 +2,24 @@
 
 const { Client } = require("pg"); 
 
-class PGDatabase {
-    constructor(config){
-        this.client;
-        this.config = config;
-        
-        this.connectServer(); 
-    }
-
-    async connectServer(){
-        this.client = new Client(this.config);
-
-        try {
-            await this.client.connect();
-            console.log("[+] - PostgreSQL Connected!");
-        }catch(error){
-            console.log("[!] - Failure to connect to PostgreSQL");
-            throw new Error(error);
-        };
-    }
-
-    closeConnection(){
-        this.client.end();
-    }
-};
-
-module.exports = new PGDatabase({
+const client = new Client({
     user: process.env.PG_USER,
     host: process.env.PG_HOST, 
     database: process.env.PG_DATABASE, 
     password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT 
-}).client;
+    port: process.env.PG_PORT
+});
+
+(async () => {
+
+    try {
+        await client.connect();
+        console.log("[+] - PostgreSQL Connected!");
+    }catch(error){
+        console.log("[!] - Failure to connect to PostgreSQL");
+        throw new Error(error);
+    };
+
+})();
+
+module.exports = client;
--- src/databases/redis/index.js
+++ src/databases/redis/index.js
@@ -1,22 +1,15 @@
 "use strict";
 
-const redis = require("redis");
+const { createClient } = require("redis");
 
-class Redis {
-    constructor(port){
-        this.client = new redis.createClient({
-            port: port
-        });
-        
-        this.createConnection();
-    }
+const client = new createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+})
 
-    createConnection(){
-        this.client.on("connect", (error) => {
-            if (error) throw new Error(error);
-            console.log("\n[+] - Redis Connected!");
-        });
-    }
-};
+client.on("connect", (error) => {
+    if (error) throw new Error(error);
+    console.log("\n[+] - Redis Connected!");
+});
 
-module.exports = new Redis(6379).client;
+module.exports = client;
